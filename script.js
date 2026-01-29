efc6acfe1aaf64f7dd4d361dfc9a712c228ba764
joaolucasgerador-code <joaolucasgerador@gmail.com>
2026-01-28 21:24:40 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const themeToggle = document.querySelector(".theme-loggle");
-
-const toggletheme = () => {
-  const isDarkTheme = document.body.classList.toggle("dark-theme");
-  themeToggle.querySelector("i").className = isDarkTheme ? "bx bxs-moon" : "bx bxs-sun";
-}
-
-themeToggle.addEventListener("click", toggletheme);
